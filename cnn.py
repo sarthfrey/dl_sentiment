21be1c7611ad9480f4a4760a26db6493f54ae080
sarthfrey <sarth.frey@gmail.com>
2017-05-16 23:40:47 -0400

    embedding layer

--- cnn.py
+++ cnn.py
@@ -1,1 +1,17 @@
-print 'hello world'
+import tensorflow as tf
+import numpy as np
+
+
+class TextCNN(object):
+		def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters):
+			# Placeholders for input, output and dropout
+			self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+			self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+			self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+			with tf.device('/cpu:0'), td.name_scope("embedding"):
+				W = tf.Variable(
+					tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+					name="W")
+				self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
+				self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
